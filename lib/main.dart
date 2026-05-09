9e119bea4204045acbf0272ee032d0f32a0d80de
Duy Nguyen <duynguyen@gnha.vn>
2026-05-09 16:52:53 +0700

    feat: designed and hanled create UI screen

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/screens/create/create.dart';
+import 'package:flutter_rpg/screens/home/home.dart';
+import 'package:flutter_rpg/theme.dart';
+
+void main() {
+  runApp(MaterialApp(theme: primaryTheme, home: const Create()));
+}
+
+class SandBox extends StatelessWidget {
+  const SandBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SandBox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: const Text('sandbox'),
+    );
+  }
+}
