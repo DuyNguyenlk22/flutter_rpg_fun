6d1e5529afacdd5e26ee9a11e613504ec3c29816
Duy Nguyen <nguyenduylk202@gmail.com>
2026-05-13 21:11:00 +0700

    feat: create new charactor , selected character , handle show dialog error input

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/character.dart';
 import 'package:flutter_rpg/models/vocation.dart';
 import 'package:flutter_rpg/screens/create/vocation_card.dart';
+import 'package:flutter_rpg/screens/home/home.dart';
 import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:uuid/uuid.dart';
+
+var uuid = Uuid();
 
 class Create extends StatefulWidget {
   const Create({super.key});
@@ -24,18 +29,75 @@ class _CreateState extends State<Create> {
     super.dispose();
   }
 
+  // handling vocation selection
+  Vocation selectedVocation = Vocation.junkie;
+
+  void updateVocation(Vocation vocation) {
+    setState(() {
+      selectedVocation = vocation;
+    });
+  }
+
   // submit handler
   void handleSubmit() {
     if (_nameController.text.trim().isEmpty) {
-      print('name must not be empty');
+      //show error dialog
+      showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            title: const StyledHeading('Missing Character Name'),
+            content: const StyledText(
+              "Every good RPG character needs a great name...",
+            ),
+            actions: [
+              StyledButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const StyledHeading('close'),
+              ),
+            ],
+            actionsAlignment: MainAxisAlignment.center,
+          );
+        },
+      );
+
       return;
     }
     if (_sloganController.text.trim().isEmpty) {
-      print('slogan must not be empty');
+      showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            title: const StyledHeading('Missing a slogan'),
+            content: const StyledText("Remember to add a catchy slogan..."),
+            actions: [
+              StyledButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const StyledHeading('close'),
+              ),
+            ],
+            actionsAlignment: MainAxisAlignment.center,
+          );
+        },
+      );
+
       return;
     }
-    print(_nameController.text);
-    print(_sloganController.text);
+
+    characters.add(
+      Character(
+        id: uuid.v4(),
+        name: _nameController.text.trim(),
+        slogan: _sloganController.text.trim(),
+        vocation: selectedVocation,
+      ),
+    );
+
+    Navigator.push(context, MaterialPageRoute(builder: (ctx) => const Home()));
   }
 
   @override
@@ -90,10 +152,31 @@ class _CreateState extends State<Create> {
               const SizedBox(height: 30),
 
               // vocation cards
-              const VocationCard(vocation: Vocation.junkie),
-              const VocationCard(vocation: Vocation.ninja),
-              const VocationCard(vocation: Vocation.raider),
-              const VocationCard(vocation: Vocation.wizard),
+              VocationCard(
+                selected: selectedVocation == Vocation.junkie,
+                onTap: updateVocation,
+                vocation: Vocation.junkie,
+              ),
+              VocationCard(
+                selected: selectedVocation == Vocation.ninja,
+                onTap: updateVocation,
+                vocation: Vocation.ninja,
+              ),
+              VocationCard(
+                selected: selectedVocation == Vocation.raider,
+                onTap: updateVocation,
+                vocation: Vocation.raider,
+              ),
+              VocationCard(
+                selected: selectedVocation == Vocation.wizard,
+                onTap: updateVocation,
+                vocation: Vocation.wizard,
+              ),
+
+              Center(child: Icon(Icons.code, color: AppColors.primaryColor)),
+              const Center(child: StyledHeading("Good Luck.")),
+              const Center(child: StyledText("And enjoy the journey....")),
+              const SizedBox(height: 30),
 
               Center(
                 child: StyledButton(
--- lib/screens/create/vocation_card.dart
+++ lib/screens/create/vocation_card.dart
@@ -1,36 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/models/vocation.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:flutter_rpg/theme.dart';
 
 class VocationCard extends StatelessWidget {
-  const VocationCard({super.key, required this.vocation});
+  const VocationCard({
+    super.key,
+    required this.vocation,
+    required this.onTap,
+    required this.selected,
+  });
 
   final Vocation vocation;
+  final bool selected;
+  final void Function(Vocation vocation) onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Row(
-          children: [
-            // vocation img
-            Image.asset(
-              'assets/img/vocations/${vocation.image}',
-              width: 80,
-              colorBlendMode: BlendMode.color,
-            ),
-            const SizedBox(width: 10),
-
-            Expanded(
-              child: Column(
-                children: [
-                  StyledHeading(vocation.title),
-                  StyledText(vocation.description),
-                ],
+    return GestureDetector(
+      onTap: () {
+        onTap(vocation);
+      },
+      child: Card(
+        color: selected ? AppColors.secondaryColor : Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            children: [
+              // vocation img
+              Image.asset(
+                'assets/img/vocations/${vocation.image}',
+                width: 80,
+                colorBlendMode: BlendMode.color,
+                color: !selected
+                    ? Colors.black.withValues(alpha: 0.8)
+                    : Colors.transparent,
               ),
-            ),
-          ],
+              const SizedBox(width: 10),
+
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    StyledHeading(vocation.title),
+                    StyledText(vocation.description),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
