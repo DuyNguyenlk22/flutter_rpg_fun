e9a5439d7b907732d84b58d0871739b2f5302700
Duy Nguyen <nguyenduylk202@gmail.com>
2026-05-18 22:52:53 +0700

    feat: dynamic data for profile page

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,20 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/character.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:flutter_rpg/theme.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({super.key});
+  const Profile({super.key, required this.character});
+
+  final Character character;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const StyledTitle("character name"),
-        centerTitle: true,
-      ),
-      body: const SingleChildScrollView(
+      appBar: AppBar(title: StyledTitle(character.name), centerTitle: true),
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [],
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              color: AppColors.secondaryColor.withValues(alpha: 0.3),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/img/vocations/${character.vocation.image}',
+                    width: 140,
+                    height: 140,
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StyledHeading(character.vocation.title),
+                        StyledText(character.vocation.description),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 20),
+            Center(child: Icon(Icons.code, color: AppColors.primaryColor)),
+
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                color: AppColors.secondaryColor.withValues(alpha: 0.5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const StyledHeading("Slogan"),
+                    StyledText(character.slogan),
+                    const SizedBox(height: 10),
+                    const StyledHeading("Weapon of Choice"),
+                    StyledText(character.vocation.weapon),
+                    const SizedBox(height: 10),
+                    const StyledHeading("Unique Ability"),
+                    StyledText(character.vocation.ability),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
